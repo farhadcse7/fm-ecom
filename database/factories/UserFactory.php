66de8ce375d5c53d3e271f988f3ec3c50cf7d616
farhadcse7 <fmfarhad23@gmail.com>
2023-11-30 02:20:29 +0600

    Customer authentication and redirect to customer dashboard

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,9 +22,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => fake()->safeEmail(),
+            'email' => fake()->phoneNumber(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use App\Http\Controllers\Backend\CouponController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Frontend\CustomerController;
 use App\Http\Controllers\Backend\TestimonialController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\Frontend\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +31,19 @@ Route::prefix('')->group(function(){
     Route::get('/shopping-cart', [CartController::class, 'cartPage'])->name('cart.page');
     Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('add-to.cart');
     Route::get('/remove-from-cart/{cart_id}', [CartController::class, 'removeFromCart'])->name('removefrom.cart');
+
+     /*Authentication routes for Customer/Guest */
+     Route::get('/register', [RegisterController::class, 'registerPage'])->name('register.page');
+     Route::post('/register', [RegisterController::class, 'registerStore'])->name('register.store');
+     Route::get('/login', [RegisterController::class, 'loginPage'])->name('login.page');
+     Route::post('/login', [RegisterController::class, 'loginStore'])->name('login.store');
+
+     Route::prefix('customer/')->middleware('auth')->group(function(){
+         Route::get('dashboard',[CustomerController::class, 'dashboard'])->name('customer.dashboard');
+         Route::get('logout', [RegisterController::class, 'logout'])->name('customer.logout');
+     });
+      /*Authentication routes for Customer/Guest end*/
+
 });
 
 
