80c485f4b476be7ca4da7b6c0dd46a3b969ea6ed
farhadcse7 <fmfarhad23@gmail.com>
2023-11-30 21:47:17 +0600

    Coupon Apply and remove on authenticated custommer

--- resources/views/frontend/pages/shopping-cart.blade.php
+++ resources/views/frontend/pages/shopping-cart.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <form action="http://themepresss.com/tf/html/tohoney/cart">
+                    {{-- <form action="http://themepresss.com/tf/html/tohoney/cart"> --}}
                         <table class="table-responsive cart-wrap">
                             <thead>
                                 <tr>
@@ -61,23 +61,41 @@
                                     <h3>Coupon</h3>
                                     <p>Enter Your Cupon Code if You Have One</p>
                                     <div class="cupon-wrap">
-                                        <input type="text" placeholder="Cupon Code">
-                                        <button>Apply Coupon</button>
+                                        <form action="{{ route('customer.couponapply') }}" method="post">
+                                            @csrf
+                                            <input type="text" name="coupon_name" placeholder="Cupon Code" class="form-control">
+                                            <button type="submit" class="btn btn-danger">Apply Coupon</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 offset-xl-5 col-lg-4 offset-lg-3 col-md-6">
                                 <div class="cart-total text-right">
                                     <h3>Cart Totals</h3>
+
+                                    <p>
+                                        @if (Session::has('coupon'))
+                                        <a class="p-1" href="{{ route('customer.couponremove', 'coupon_name') }}"> <i class="fa fa-times">
+                                        </i></a>
+
+                                        <b> {{ Session::get('coupon')['name'] }} </b> is Applied
+                                        @endif
+                                    </p>
+
                                     <ul>
-                                        <li><span class="pull-left">Subtotal </span>${{ $total_price }}</li>
-                                        <li><span class="pull-left"> Total </span> ${{ $total_price }}</li>
+                                         @if (Session::has('coupon'))
+                                          <li><span class="pull-left">Discount Amount: </span>৳{{ Session::get('coupon')['discount_amount'] }}</li>
+                                          <li><span class="pull-left"> Total: </span>৳ {{ Session::get('coupon')['balance'] }} <del class="text-danger">৳ {{ Session::get('coupon')['cart_total'] }}</del></li>
+                                         @else
+                                          <li><span class="pull-left">Subtotal: </span>৳{{ $total_price }}</li>
+                                          <li><span class="pull-left"> Total: </span> ৳{{ $total_price }}</li>
+                                         @endif
                                     </ul>
                                     <a href="checkout.html">Proceed to Checkout</a>
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    {{-- </form> --}}
                 </div>
             </div>
         </div>
